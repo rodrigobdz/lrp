e2c10c43ba561fcb17125047707bfe69d95da823
Rodrigo Bermudez Schettino <rodrigobdz@users.noreply.github.com>
2022-05-10 14:33:43 +0200

    Lint: Fix issues in perturbation_modes/constants.py
    Fix pydoclint issues.

--- pf/perturbation_modes/constants.py
+++ pf/perturbation_modes/constants.py
@@ -1,6 +1,4 @@
-r"""Constants for perturbation techniques available for Pixel-Flipping and
-Region Perturbation Algorithms.
-"""
+r"""Constants for perturbation techniques available for Pixel-Flipping and Region Perturbation."""
 
 
 # pylint: disable=duplicate-code
@@ -12,9 +10,8 @@ __status__ = 'Development'
 # pylint: enable=duplicate-code
 
 
-class PerturbModes:
-    r"""Constants for perturbation techniques available for Pixel-Flipping
-    and Region Perturbation Algorithms.
-    """
+class PerturbModes:  # pylint: disable=too-few-public-methods
+    r"""Constants for perturbation techniques."""
+
     RANDOM: str = 'Random sampling'
     INPAINTING: str = 'Inpainting'
