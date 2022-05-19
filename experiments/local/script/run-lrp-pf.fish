f12e2db4d182abbcfecfb6eef9f36f60bd3e5575
Rodrigo Bermudez Schettino <rodrigobdz@users.noreply.github.com>
2022-05-19 15:20:06 +0200

    Experiments: Remove venv's deactivate call from fish script
    Command fails if virtualenv is active.

--- experiments/local/script/run-lrp-pf.fish
+++ experiments/local/script/run-lrp-pf.fish
@@ -5,14 +5,11 @@
 # The following variable should correspond to the value set in batch_lrp_pf.py
 set TOTAL_NUMBER_OF_EXPERIMENTS 16
 
-echo 'Deactivating virtualenv, if active'
-deactivate
-
 echo 'Activating venv for fish shell'
 source ./venv/bin/activate.fish
 
 echo "Running $TOTAL_NUMBER_OF_EXPERIMENTS experiments"
-for i in (seq 0 "$TOTAL_NUMBER_OF_EXPERIMENTS")
-    echo "Running experiment $i"
+for i in (seq 0 (math "$TOTAL_NUMBER_OF_EXPERIMENTS" - 1))
+    echo "Running experiment ID $i"
     python3 experiments/cluster/script/batch_lrp_pf.py --experiment-id "$i"
 end
