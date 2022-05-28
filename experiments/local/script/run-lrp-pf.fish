4292da5845ca9b4ec121ed6cffa4f0d99eb2dd44
Rodrigo Bermudez Schettino <rodrigobdz@users.noreply.github.com>
2022-05-28 11:36:51 +0200

    Script: Assume dependencies are already installed
    Remove env activation step because it would fail if env was already active.
    Also, it was counterintuitive that this fish script required env not to be active but
    the visualize script required the env to be active.

--- experiments/local/script/run-lrp-pf.fish
+++ experiments/local/script/run-lrp-pf.fish
@@ -5,9 +5,6 @@
 # The following variable should correspond to the value set in batch_lrp_pf.py
 set TOTAL_NUMBER_OF_EXPERIMENTS 16
 
-echo 'Activating venv for fish shell'
-source ./venv/bin/activate.fish
-
 echo "Running $TOTAL_NUMBER_OF_EXPERIMENTS experiments"
 for i in (seq 0 (math "$TOTAL_NUMBER_OF_EXPERIMENTS" - 1))
     echo "Running experiment ID $i"
