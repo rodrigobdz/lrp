ba2ac6b6b90bd29525f9a0dc8b89acc4e5be0615
Rodrigo Bermudez Schettino <rodrigobdz@users.noreply.github.com>
2022-06-24 15:34:50 +0200

    fish: Parallelize local experiment execution

--- experiments/local/script/run-lrp-pf.fish
+++ experiments/local/script/run-lrp-pf.fish
@@ -1,7 +1,6 @@
 #!/usr/bin/env fish
 #
-# Run experiments on personal machine (not cluster) sequentially.
-
+# Run experiments on personal machine (not cluster) in parallel.
 
 # Check that arguments were passed.
 if test -z "$argv[1]"
@@ -10,23 +9,32 @@ Usage: ./run_experiments.sh <config_file>"
     exit 1
 end
 
+# Export variable to parallel's subshell.
+set --export CONFIG_FILE_PATH $argv[1]
+
 # Value is read from config file.
 set TOTAL_NUMBER_OF_EXPERIMENTS (grep 'TOTAL_NUMBER_OF_EXPERIMENTS =' ./experiments/local/local.config | awk '{print $3}')
 
 echo "Running $TOTAL_NUMBER_OF_EXPERIMENTS experiments"
 
 # Execute for range of experiments (0 to TOTAL_NUMBER_OF_EXPERIMENTS - 1)
-for i in (seq 0 (math "$TOTAL_NUMBER_OF_EXPERIMENTS" - 1))
-    echo "Running experiment ID $i"
-    time ./venv/bin/python3 ./experiments/script/batch_lrp_pf.py --experiment-id "$i" --config-file "$argv[1]"
+parallel --jobs 50% --line-buffer --tag --halt-on-error now,fail=1 'begin
+    function run_experiment --argument-names experiment_id
+        echo "Running experiment ID $experiment_id"
+        time ./venv/bin/python3 ./experiments/script/batch_lrp_pf.py --experiment-id "$experiment_id" --config-file "$CONFIG_FILE_PATH"
 
-    # Check status of last experiment and exit on error
-    if test $status -ne 0
-        echo "ERORR: Experiment ID $i failed"
-        exit 1
+        # Check status of last experiment and exit on error
+        if test $status -ne 0
+            echo "ERORR: Experiment ID $experiment_id failed"
+            exit 1
+        end
     end
 end
+run_experiment {}' ::: (seq 0 (math "$TOTAL_NUMBER_OF_EXPERIMENTS" - 1))
 
-echo
-echo "Generate plots from experiment results"
-time ./venv/bin/python3 ./experiments/script/visualize.py --config-file ./experiments/local/local.config
+# Exit on error
+# Similar to bash set -o errexit, but for fish.
+# https://stackoverflow.com/a/19883639
+and echo
+and echo "Generate plots from experiment results"
+and time ./venv/bin/python3 ./experiments/script/visualize.py --config-file ./experiments/local/local.config
