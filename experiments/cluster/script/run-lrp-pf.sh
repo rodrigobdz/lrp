7a2ae7490becd0d47cd4d1288277c779ad4d4461
Rodrigo Bermudez Schettino <rodrigobdz@users.noreply.github.com>
2022-06-28 11:46:29 +0200

    Experiments: Adjust qsub parameters to range 1-256

--- experiments/cluster/script/run-lrp-pf.sh
+++ experiments/cluster/script/run-lrp-pf.sh
@@ -14,7 +14,7 @@
 #$ -l mem_free=16G    # request 16GB of free memory
 #$ -q all.q           # submit jobs to queue named all.q (general queue)
 #$ -cwd               # execute in current working directory
-#$ -t 1-10000           # start multiple instances with identified SGE_TASK_ID from 1 to n
+#$ -t 1-256           # start multiple instances with identified SGE_TASK_ID from 1 to n
 #                     # upper bound of -t should be equal to TOTAL_NUMBER_OF_EXPERIMENTS in *.config
 #
 # Submit jobs to cluster using qsub
